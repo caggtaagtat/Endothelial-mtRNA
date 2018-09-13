3beb3d9ae56740b533e28933bcc745a6e6b72ea3
caggtaagtat <38690958+caggtaagtat@users.noreply.github.com>
2018-09-13 13:21:37 +0200

    Update QA.sh

--- QA.sh
+++ QA.sh
@@ -4,3 +4,14 @@
 
 #Doing FastQC with unzipped FASTQ files and storing the result in new folder
 /path_to/FastQC/fastqc -o /path_to/Project/QA/unprocessed/ /path_to/Project/FASTQ_unprocessed/*.fq
+
+#Generating single MultiQC report from all generated FATQC reports
+
+#Changing workindirectory to respective folder
+cd /path_to/Project/QA/unprocessed/
+
+#Loading MultiQC
+module load multiqc
+
+#Summary of all FASTQC reports generated in current directory
+multiqc .
