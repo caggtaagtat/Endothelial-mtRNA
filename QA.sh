0a553549e42703f50d8f1ed50ec92137aa9ba27d
caggtaagtat <38690958+caggtaagtat@users.noreply.github.com>
2018-09-13 13:57:47 +0200

    Update QA.sh

--- QA.sh
+++ QA.sh
@@ -2,6 +2,10 @@
 #PBS -l select=1:ncpus=8:mem=100gb:arch=ivybridge
 #PBS -l walltime=71:59:59
 
+#Create new folder for FASTQC output
+cd /path_to/Project/QA/
+mkdir unprocessed
+
 #Doing FastQC with unzipped FASTQ files and storing the result in new folder
 /path_to/FastQC/fastqc -o /path_to/Project/QA/unprocessed/ /path_to/Project/FASTQ_unprocessed/*.fq
 
